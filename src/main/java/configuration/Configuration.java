9a7bcec9161c5d880acbdb7fae31286d1d22cd5c
Elad Hakmon <eladhakmon11@gmail.com>
2022-06-30 16:26:08 +0300

    Adding Asserts for test

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -1,9 +1,7 @@
-package com.amazon.automation.config;
+package configuration;
 
 public class Configuration {
     public static final String CHROME_DRIVER = "C:\\Users\\elad\\Downloads\\chromedriver_win32\\chromedriver.exe";
     public static final String CHROME_BROWSER = "webdriver.chrome.driver";
-    public static final String SEARCH_ITEM = "Samsung Galaxy";
     public static final String BASE_URL = "https://www.amazon.com/";
-
 }
--- src/test/java/amazon_tests/_001_SearchForItemTest.java
+++ src/test/java/amazon_tests/_001_SearchForItemTest.java
@@ -1,70 +1,123 @@
 package amazon_tests;
 
-import com.amazon.automation.config.Configuration;
+import configuration.Configuration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.AssertUtils;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class _001_SearchForItemTest {
 
-    //String zipCode = "90011";
+    public static final String searchItem = "Samsung Galaxy";
+    public static final String ZIP_CODE = "90011";
     public static final By TOP_SEARCH_BOX_ID = By.id("twotabsearchtextbox");
     public static final By SEARCH_BUTTON = By.id("nav-search-submit-button");
     public static final By ALL_RETURNED_ELEMENT = By.xpath("//div[@data-component-type='s-search-result']//h2");
     public static final By ADD_TO_CART = By.id("add-to-cart-button");
-    public static final By SELECTED_PHONE_DESCRIPTION = By.xpath("//span[@class='a-truncate-cut']");
     public static final By LIST_OF_RESULTS_LINKS = By.xpath("//h2[contains(@class,'a-size-mini a-spacing-none a-color-base s-line-clamp-2')]/a");
     public static final By GLOBAL_LOCATION = By.id("nav-global-location-popover-link");
     public static final By ZIP_CODE_INPUT = By.id("GLUXZipUpdateInput");
-    public static final By ZIP_CODE_DONE_BUTTON = By.id("a-autoid-3-announce");
     public static final By ZIP_CODE_APPLY_BUTTON = By.xpath("//input[@aria-labelledby='GLUXZipUpdate-announce']");
+    public static final By ZIP_CODE_CONFIRM_BUTTON = By.xpath("//*[@class='a-popover-footer']//*[@id='GLUXConfirmClose']");
+    public static final By CART_COUNT = By.id("nav-cart-count");
+    public static final By PRICE_LIST = By.xpath("//div[@class='a-row a-size-base a-color-base']//span[@class='a-price-whole']");
+    public static final By PRICE_FRACTION_LIST = By.xpath("//div[@class='a-row a-size-base a-color-base']//span[@class='a-price-fraction']");
+    public static final By NO_THANKS_BUTTON = By.xpath("//input[@aria-labelledby='attachSiNoCoverage-announce']");
+    public static final By CLOSE_SIDE_BUTTON = By.id("attach-close_sideSheet-link");
+    public static final By SUBTOTAL_PRICE = By.xpath("(//span[@class='a-size-medium a-color-base sc-price sc-white-space-nowrap'])[1]");
+    public static final By NAV_CART_BUTTON = By.id("nav-cart");
 
     @Test
     public void _001_SearchForItem() {
-        try {
-            System.setProperty(Configuration.CHROME_BROWSER, Configuration.CHROME_DRIVER);
-            WebDriver driver = new ChromeDriver();
-            // Implicitly Wait
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            driver.get(Configuration.BASE_URL);
-            driver.manage().window().maximize();
+        System.setProperty(Configuration.CHROME_BROWSER, Configuration.CHROME_DRIVER);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get(Configuration.BASE_URL);
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-            // Explicit Wait
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            /*
-            driver.findElement(GLOBAL_LOCATION).click();
-            driver.findElement(ZIP_CODE_INPUT).sendKeys(zipCode);
-            driver.findElement(ZIP_CODE_APPLY_BUTTON).click();
-            driver.findElement(ZIP_CODE_DONE_BUTTON).click();
-            */
-            driver.findElement(TOP_SEARCH_BOX_ID).sendKeys(Configuration.SEARCH_ITEM);
-            driver.findElement(SEARCH_BUTTON).click();
+        driver.findElement(GLOBAL_LOCATION).click();
+        driver.findElement(ZIP_CODE_INPUT).sendKeys(ZIP_CODE);
+        driver.findElement(ZIP_CODE_APPLY_BUTTON).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ZIP_CODE_CONFIRM_BUTTON));
+        driver.findElement(ZIP_CODE_CONFIRM_BUTTON).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(TOP_SEARCH_BOX_ID));
+        driver.findElement(TOP_SEARCH_BOX_ID).sendKeys(searchItem);
+        driver.findElement(SEARCH_BUTTON).click();
 
-            List<WebElement> searchResults = driver.findElements(ALL_RETURNED_ELEMENT);
-            System.out.println("Print the Results:");
-            searchResults.forEach(result -> System.out.println(result.getText()));
-
-//            int randomNumber = (int) ((Math.random() * (searchResults.size() - 0)));
-            WebElement randomElement = searchResults.get(0);
-            String randomElementDes = randomElement.getText();
-            System.out.println("the selected phone is: " + randomElementDes);
-            List<WebElement> linksFromResult = driver.findElements(LIST_OF_RESULTS_LINKS);
-            linksFromResult.get(0).click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(ADD_TO_CART));
-            driver.findElement(ADD_TO_CART).click();
-//            String descriptionForSelectedPhone = driver.findElement(SELECTED_PHONE_DESCRIPTION).getText();
-        } catch (Exception e){
-            System.out.println(e.getMessage());
+        // Verify cart contains 0 Products
+        int cartCounter = Integer.parseInt(driver.findElement(CART_COUNT).getText());
+        AssertUtils.assertEquals(0, cartCounter, "Verify cart contains 0 Products Before adding one of " + searchItem);
+        List<WebElement> listPriceWE = driver.findElements(PRICE_LIST);
+        List<Double> listPrice = new ArrayList<>();
+        for (WebElement lp : listPriceWE) {
+            if (lp.getText().replace(",","").equals("")) {
+                listPrice.add(0.0);
+            } else {
+                listPrice.add(Double.valueOf(lp.getText().replace(",","")));
+            }
         }
+        List<WebElement> listPriceFractionWE = driver.findElements(PRICE_FRACTION_LIST);
+        List<Double> listPriceFraction = new ArrayList<>();
+        for (WebElement lpFraction : listPriceFractionWE) {
+            if (lpFraction.getText().equals("") || Double.valueOf(lpFraction.getText()) == 00) {
+                listPriceFraction.add(0.0);
+            } else {
+               double res = Double.valueOf(lpFraction.getText()) / 100;
+                listPriceFraction.add(res);
+            }
+        }
+        List<Double> finalPriceList = new ArrayList<>();
+        for (int i = 0; i < listPrice.size(); i++) {
+            if(listPrice.get(i) + listPriceFraction.get(i) > 0.0){
+                finalPriceList.add(listPrice.get(i) + listPriceFraction.get(i));
+            }
+        }
+        List<String> searchResultsTitles = driver.findElements(ALL_RETURNED_ELEMENT).stream()
+                .map(res -> res.getText())
+                .collect(Collectors.toList());
+        int randomNumber = (int) ((Math.random() * (searchResultsTitles.size() - 0)));
+        List<WebElement> linksFromResult = driver.findElements(LIST_OF_RESULTS_LINKS);
+        linksFromResult.get(randomNumber).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(ADD_TO_CART));
+        driver.findElement(ADD_TO_CART).click();
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(NO_THANKS_BUTTON));
+            driver.findElement(NO_THANKS_BUTTON).click();
+        } catch (Exception e){
+            System.out.println("Probably selected a product that does not require to use NO_THANKS button...");
+        }
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(CLOSE_SIDE_BUTTON));
+            driver.findElement(CLOSE_SIDE_BUTTON).click();
+        } catch (Exception e){
+            System.out.println("Probably selected a product that does not require to use CLOSE_SIDE button...");
+        }
+
+        // Verify cart contains 1 Products
+        wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(CART_COUNT), "1"));
+        cartCounter = Integer.parseInt(driver.findElement(CART_COUNT).getText());
+        AssertUtils.assertEquals(1, cartCounter, "Verify cart contains 1 Product After adding one " + searchItem + " to cart");
+
+        // Verify the title of the selected Product match to at least one title from the returned Products list
+        String randomElementTitle = searchResultsTitles.get(randomNumber);
+        System.out.println("the selected phone is: " + randomElementTitle);
+        boolean checkIfTitleExist = searchResultsTitles.contains(randomElementTitle);
+        AssertUtils.assertTrue(checkIfTitleExist, "Verify the title of the selected Product exist in Results list");
+
+        // Verify the price on cart is bigger than 0
+        driver.findElement(NAV_CART_BUTTON).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(SUBTOTAL_PRICE));
+        boolean subtotalPrice = Double.valueOf(driver.findElement(SUBTOTAL_PRICE).getText().replace("$","")) > 0;
+        AssertUtils.assertTrue(subtotalPrice,"Verify the price on cart is bigger than 0");
     }
 }
